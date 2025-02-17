316181c56e4c2d655bfb9a6e64ebbaba2e29ef73
Ben-1819 <ben.brown20332@gmail.com>
2025-02-17 10:28:13 +0000

    Added migrations for owners, dogs, kennels and bookings

---
+++ database/migrations/2025_02_17_101236_create_dogs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dogs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')->constrained('owners');
+            $table->string('name');
+            $table->string('Breed');
+            $table->string('size');
+            $table->integer('age');
+            $table->string('training_level');
+            $table->string('temperment');
+            $table->string('extra_info');
+            $table->integer('feed_per_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dogs');
+    }
+};
