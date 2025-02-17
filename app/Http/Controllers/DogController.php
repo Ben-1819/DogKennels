00857d50daf6c4e529b00037a9187fbea0b8877e
Ben-1819 <ben.brown20332@gmail.com>
2025-02-17 12:00:19 +0000

    Methofd for CRUD added to dog controller

--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Dog;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
 class DogController extends Controller
@@ -12,7 +13,10 @@ class DogController extends Controller
      */
     public function index()
     {
-        //
+        log::info('Setting variable all_dogs');
+        $all_dogs = Dog::all();
+        log::info('Returning to view dog.index and passing all_dogs as parameter');
+        return view('dog.index', compact('all_dogs'));
     }
 
     /**
@@ -20,7 +24,8 @@ class DogController extends Controller
      */
     public function create()
     {
-        //
+        log::info('Showing view to create new dog');
+        return view('dog.create');
     }
 
     /**
@@ -28,23 +33,62 @@ class DogController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'owner_id' => ['required', 'integer'],
+            'name' => ['required', 'string'],
+            'breed' => ['required', 'string'],
+            'size' => ['required', 'string'],
+            'age' => ['required', 'integer'],
+            'training_level' => ['required', 'string'],
+            'temperment' => ['required', 'string'],
+            'extra_info' => ['string'],
+            'feed_per_day' => ['required', 'integer']
+        ]);
+
+        log::info('Creating new dog');
+        $dog = new Dog([
+            'owner_id' => $request->owner_id,
+            'name' => $request->name,
+            'breed' => $request->breed,
+            'size' => $request->size,
+            'age' => $request->age,
+            'training_level' => $request->training_level,
+            'temperment' => $request->temperment,
+            'extra_info' => $request->extra_info,
+            'feed_per_day' => $request->feed_per_day
+        ]);
+        $dog->save();
+        log::info('Created new dog');
+        log::info('Dog owner: {owner}', ['owner' => $request->owner_id]);
+        log::info('Dog name: {name}', ['name' => $request->name]);
+        log::info('Dog breed: {breed}', ['breed' => $request->breed]);
+        log::info('Dog size: {size}', ['size' => $request->size]);
+        log::info('Dog age: {age}', ['age' => $request->age]);
+        log::info('Dog training level: {training_level}', ['training_level' => $request->training_level]);
+        log::info('Extra info: {extra_info}', ['extra_info' => $request->extra_info]);
+        log::info('Times to feed per day: {feed_per_day}', ['feed_per_day' => $request->feed_per_day]);
+        log::info('Returning to dog index');
+        return redirect()->route('dog.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Dog $dog)
+    public function show(Dog $dog, $id)
     {
-        //
+        log::info('Showing dog.show for dog with id: {id}', ['id' => $id]);
+        $dog = Dog::find($id);
+        return view('dog.show', compact('dog'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Dog $dog)
+    public function edit(Dog $dog, $id)
     {
-        //
+        log::info('Showing dog.edit for dog with id: {id}', ['id' => $id]);
+        $dog = Dog::find($id);
+        return view('dog.find', compact('dog'));
     }
 
     /**
@@ -52,14 +96,52 @@ class DogController extends Controller
      */
     public function update(Request $request, Dog $dog)
     {
-        //
+        $request->validate([
+            'owner_id' => ['required', 'integer'],
+            'name' => ['required', 'string'],
+            'breed' => ['required', 'string'],
+            'size' => ['required', 'string'],
+            'age' => ['required', 'integer'],
+            'training_level' => ['required', 'string'],
+            'temperment' => ['required', 'string'],
+            'extra_info' => ['string'],
+            'feed_per_day' => ['required', 'integer']
+        ]);
+
+        log::info('Updating dog with id: {id}', ['id' => $request->id]);
+        $update_dog = Dog::where('id', $request->id)->update([
+            'owner_id' => $request->owner_id,
+            'name' => $request->name,
+            'breed' => $request->breed,
+            'size' => $request->size,
+            'age' => $request->age,
+            'training_level' => $request->training_level,
+            'temperment' => $request->temperment,
+            'extra_info' => $request->extra_info,
+            'feed_per_day' => $request->feed_per_day
+        ]);
+
+        log::info('Updated dog');
+        log::info('Dog owner: {owner}', ['owner' => $request->owner_id]);
+        log::info('Dog name: {name}', ['name' => $request->name]);
+        log::info('Dog breed: {breed}', ['breed' => $request->breed]);
+        log::info('Dog size: {size}', ['size' => $request->size]);
+        log::info('Dog age: {age}', ['age' => $request->age]);
+        log::info('Dog training level: {training_level}', ['training_level' => $request->training_level]);
+        log::info('Extra info: {extra_info}', ['extra_info' => $request->extra_info]);
+        log::info('Times to feed per day: {feed_per_day}', ['feed_per_day' => $request->feed_per_day]);
+        log::info('Returning to dog index');
+        return redirect()->route('dog.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Dog $dog)
+    public function destroy(Dog $dog, $id)
     {
-        //
+        log::info('Deleting dog with id: {id}', ['id' => $id]);
+        Dog::where('id',$id)->delete();
+        log::info('Dog deleted, returning to dog index');
+        return redirect()->route('dog.index');
     }
 }
